245c12e9d764d68296e905f4d10c449507f470d6
Gerey <tatarchanov@mail.ru>
2024-01-09 11:19:17 +0300

    base buttom isEnabled property  added

--- lib/widget_gallery/base_button.dart
+++ lib/widget_gallery/base_button.dart
@@ -5,11 +5,13 @@ class BaseButton extends StatelessWidget {
   const BaseButton({
     super.key,
     this.svgIconName,
+    this.isEnabled = true,
     this.usePrefix = false,
     required this.text,
     required this.onPressed,
   });
 
+  final bool isEnabled;
   final String text;
   final VoidCallback onPressed;
   final bool usePrefix;
@@ -17,40 +19,49 @@ class BaseButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(8),
-          color: Theme.of(context).colorScheme.inversePrimary),
-      child: ElevatedButton(
-          onPressed: onPressed,
-          style: ButtonStyle(
-            overlayColor: MaterialStatePropertyAll(
-                Theme.of(context).primaryColor.withOpacity(0.1)),
-            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-              RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(8),
-              ),
-            ),
-            backgroundColor: const MaterialStatePropertyAll(Colors.transparent),
-            padding: const MaterialStatePropertyAll(
-                EdgeInsets.symmetric(vertical: 14)),
-            elevation: const MaterialStatePropertyAll(0),
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              usePrefix
-                  // ? SvgPicture.asset('${Constants.svgPath}$svgIconName')
-                  ? const Icon(Icons.abc)
-                  : const SizedBox(),
-              const SizedBox(width: 4),
-              //AutoSizeText
-              Text(
-                text,
-                style: const TextStyle(color: Colors.white),
-              ),
-            ],
-          )),
-    );
+    return TweenAnimationBuilder(
+        tween: ColorTween(
+            begin: Colors.grey.withOpacity(0.6),
+            end: isEnabled
+                ? Theme.of(context).colorScheme.inversePrimary
+                : Colors.grey.withOpacity(0.6)),
+        duration: const Duration(milliseconds: 800),
+        builder: (context, color, child) {
+          return Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8), color: color),
+            child: ElevatedButton(
+                onPressed: isEnabled ? onPressed : null,
+                style: ButtonStyle(
+                  overlayColor:
+                      MaterialStatePropertyAll(Colors.black.withOpacity(0.1)),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                  backgroundColor:
+                      const MaterialStatePropertyAll(Colors.transparent),
+                  padding: const MaterialStatePropertyAll(
+                      EdgeInsets.symmetric(vertical: 14)),
+                  elevation: const MaterialStatePropertyAll(0),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    usePrefix
+                        // ? SvgPicture.asset('${Constants.svgPath}$svgIconName')
+                        ? const Icon(Icons.abc)
+                        : const SizedBox(),
+                    const SizedBox(width: 4),
+                    //AutoSizeText
+                    Text(
+                      text,
+                      style: const TextStyle(color: Colors.white),
+                    ),
+                  ],
+                )),
+          );
+        });
   }
 }
